8702cb28e07ca8e7c9becaab6810ee1e420669f4
Shubhgupta-coder <95521631+Shubhgupta-coder@users.noreply.github.com>
2025-05-23 12:21:32 +0530

    initial commit

---
+++ src/main/java/com/cg/todo_app/todo_app/App.java
@@ -0,0 +1,13 @@
+package com.cg.todo_app.todo_app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
